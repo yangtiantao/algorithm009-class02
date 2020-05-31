e58a0f2c0f58bb43195fa5067c6497abf928f429
Threads <279977341@qq.com>
2020-05-31 23:09:41 +0800

    threeSum

---
+++ Week_01/ThreeSum.java
@@ -0,0 +1,32 @@
+class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if(null == nums || nums.length < 3){
+            return result;
+        }
+        Arrays.sort(nums);
+        for (int i=0;i<nums.length-2;i++){
+            if(nums[i]>0){
+                break;
+            }
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int a=i+1,b=nums.length-1;
+            while(a<b){
+                int sum = nums[i] + nums[a] + nums[b];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i],nums[a],nums[b]));
+                    while(a<b && nums[a] == nums[a+1]) a++;
+                    while(a<b && nums[b] == nums[b-1]) b--;
+                    a++;
+                    b--;
+                } else if(sum < 0) {
+                    a++;
+                } else if(sum > 0){
+                    b--;
+                }
+            }
+            
+        }
+        return result;
+    }
+}
